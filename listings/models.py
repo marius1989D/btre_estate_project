07b305cbb5f4cfe35301a49774c46194f348d944
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-03-14 21:46:52 +0000

    registered listing and realtors on admin panel and added media files

--- listings/models.py
+++ listings/models.py
@@ -4,6 +4,7 @@ from realtors.models import Realtor
 
 class Listing(models.Model):
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
