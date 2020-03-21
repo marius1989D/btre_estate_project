8429ba95e13b416cc018d01eaf1b02067665028b
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-03-21 22:49:33 +0000

    added search template and serach functionality

--- pages/views.py
+++ pages/views.py
@@ -1,23 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 from listings.models import Listing
 from realtors.models import Realtor
 
 
 def index(request):
-    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3] #pylint: disable=maybe-no-member
     context = {
-        'listings': listings
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
     }
     return render(request, 'pages/index.html', context)
 
 def about(request):
     # get all realtors
-    realtors = Realtor.objects.order_by('-hire_date')
+    realtors = Realtor.objects.order_by('-hire_date') #pylint: disable=maybe-no-member
 
     # get MVP
-    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True) #pylint: disable=maybe-no-member
     context = {
         'realtors': realtors,
         'mvp_realtors': mvp_realtors
