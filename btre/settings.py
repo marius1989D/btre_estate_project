f7f734275e0d738e8c4245fe82b1f93be2c0bf4b
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-03-24 23:05:32 +0000

    removed email from settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -147,12 +147,6 @@ MESSAGE_TAGS = {
 
 # Email config
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
-
 try:
     from .local_settings import *
 except ImportError:
