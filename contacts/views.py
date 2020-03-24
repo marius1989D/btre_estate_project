57b66bd9f1c2a38e8bb3753c3b87d4ce4b3e2254
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-03-24 20:41:54 +0000

    added send email functionality to get notified when a user request details about a property

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 
 from .models import Contact
 
@@ -14,6 +15,15 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+        #check if usser has made inquiry already
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id) #pylint: disable=maybe-no-member
+            if has_contacted:
+                messages.error(request, 'You have already made an inquiry for this listing')
+                return redirect('/listings/'+listing_id)
+
+
         contact = Contact(listing=listing, 
                         listing_id=listing_id, 
                         name=name, email=email, 
@@ -22,6 +32,15 @@ def contact(request):
                         user_id=user_id)
 
         contact.save()
+
+        #send email
+        send_mail(
+            'Property Listing Inquiry',
+            'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info.',
+            'creion.m@gmail.com',
+            [realtor_email, 'marius_suk2000@yahoo.com'],
+            fail_silently=False
+        )
 
         messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
         return redirect('/listings/'+listing_id)
