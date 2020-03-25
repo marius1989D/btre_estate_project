525aff040c3026670bfb25d06e945beb85f1a40c
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-03-25 00:28:16 +0000

    removed senf mail function from viiews.contacts

--- contacts/views.py
+++ contacts/views.py
@@ -34,13 +34,13 @@ def contact(request):
         contact.save()
 
         #send email
-        send_mail(
-            'Property Listing Inquiry',
-            'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info.',
-            'creion.m@gmail.com',
-            [realtor_email, 'marius_suk2000@yahoo.com'],
-            fail_silently=False
-        )
+        #send_mail(
+        #    'Property Listing Inquiry',
+        #    'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info.',
+        #    'creion.m@gmail.com',
+        #    [realtor_email, 'marius_suk2000@yahoo.com'],
+        #    fail_silently=False
+        #)
 
         messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
         return redirect('/listings/'+listing_id)
